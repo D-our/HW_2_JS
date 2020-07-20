0abf388eeb68de7681f8316b4cc9e9ae79d3d163
Anna Potyomkina <64991134+D-our@users.noreply.github.com>
2020-07-20 12:42:38 +0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,44 @@
+
+let score = 0;
+
+    let countTwo = prompt('Сколько будет 2+2?');
+
+        if (countTwo == 4) {
+            score = score + 10;
+        };
+
+    let sunRise = prompt('Солнце встает на востоке?');
+
+        if (sunRise == 'да') {
+            score = score + 10;
+        };
+
+    let countFive = prompt('Сколько будет 5 / 0?');
+
+        if (countFive == 0) {
+            score = score + 10;
+        };
+
+    let skyColor= prompt('Какого цвета небо?');
+
+        if (skyColor == 'Голубое') {
+            score = score + 10;
+        };
+
+    let waterBoils = prompt('При какой температуре закипает вода?');
+
+        if (waterBoils == '100') {
+            score = score + 10;
+        };
+
+
+    alert(score + ' баллов');
+
+
+
+
+
+
+
+
+
